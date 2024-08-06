6f80ba78092a006e26bf8232d90f196d7a315398
Roshan Varughese <40583749+Animeboynz@users.noreply.github.com>
2024-08-07 00:06:16 +1200

    Removing Madara

--- JSON-Extension/src/en/jsonext/src/eu/kanade/tachiyomi/extension/en/jsonext/JSONExt.kt
+++ JSON-Extension/src/en/jsonext/src/eu/kanade/tachiyomi/extension/en/jsonext/JSONExt.kt
@@ -1,23 +1,31 @@
-package eu.kanade.tachiyomi.extension.en.devext
+package eu.kanade.tachiyomi.extension.en.jsonext
 
 import android.util.Log
-import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.online.ParsedHttpSource
 import okhttp3.Request
 import org.json.JSONArray
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 import rx.Observable
 import java.io.IOException
 
-class DevExt : Madara(
-    "Dev Extension",
-    "https://mihon-dev.netlify.app",
-    "en",
-) {
+class JSONExt : ParsedHttpSource() {
+    override val name = "JSON Extension"
+    final override val lang = "en"
+    override val baseUrl = "https://mihon-dev.netlify.app"
     override val supportsLatest = false
+    override fun chapterFromElement(element: Element): SChapter {
+        TODO("Not yet implemented")
+    }
+
+    override fun chapterListSelector(): String {
+        TODO("Not yet implemented")
+    }
 
     private val data = fetchJson("https://mihon-dev.netlify.app/mangas.json")
 
@@ -47,6 +55,66 @@ class DevExt : Madara(
         return fetchPopularManga(page)
     }
 
+    override fun imageUrlParse(document: Document): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun latestUpdatesFromElement(element: Element): SManga {
+        TODO("Not yet implemented")
+    }
+
+    override fun latestUpdatesNextPageSelector(): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun latestUpdatesRequest(page: Int): Request {
+        TODO("Not yet implemented")
+    }
+
+    override fun latestUpdatesSelector(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun mangaDetailsParse(document: Document): SManga {
+        TODO("Not yet implemented")
+    }
+
+    override fun pageListParse(document: Document): List<Page> {
+        TODO("Not yet implemented")
+    }
+
+    override fun popularMangaFromElement(element: Element): SManga {
+        TODO("Not yet implemented")
+    }
+
+    override fun popularMangaNextPageSelector(): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun popularMangaRequest(page: Int): Request {
+        TODO("Not yet implemented")
+    }
+
+    override fun popularMangaSelector(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun searchMangaFromElement(element: Element): SManga {
+        TODO("Not yet implemented")
+    }
+
+    override fun searchMangaNextPageSelector(): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        TODO("Not yet implemented")
+    }
+
+    override fun searchMangaSelector(): String {
+        TODO("Not yet implemented")
+    }
+
     private fun fetchJson(url: String): String? {
         val request = Request.Builder()
             .url(url)
@@ -58,8 +126,7 @@ class DevExt : Madara(
                 Log.e("Animeboynz", "Failed to fetch JSON: ${response.message}")
                 null
             } else {
-                val json = response.body?.string()
-                JSONArray(json).toString(4) // Pretty print JSON with an indentation of 4 spaces
+                response.body?.string() // Pretty print JSON with an indentation of 4 spaces
             }
         } catch (e: IOException) {
             Log.e("Animeboynz", "Failed to fetch JSON: ${e.message}")
@@ -69,6 +136,12 @@ class DevExt : Madara(
 
     override fun fetchPopularManga(page: Int): Observable<MangasPage> {
         val mangaList = mutableListOf<SManga>()
+        // ////////
+        if (data == null) {
+            Log.e("JSONExt", "Data is null, cannot fetch popular manga")
+            return Observable.just(MangasPage(emptyList(), false))
+        }
+        // ////////
 
         try {
             val jsonArray = JSONArray(data)
@@ -94,6 +167,13 @@ class DevExt : Madara(
 
     override fun fetchChapterList(manga: SManga): Observable<List<SChapter>> {
         val chaptersList = mutableListOf<SChapter>()
+
+        // ////////
+        if (data == null) {
+            Log.e("JSONExt", "Data is null, cannot fetch chapter list")
+            return Observable.just(emptyList())
+        }
+        // /////////
 
         try {
             val jsonArray = JSONArray(data)
@@ -124,6 +204,13 @@ class DevExt : Madara(
 
     override fun fetchPageList(chapter: SChapter): Observable<List<Page>> {
         val pageList = mutableListOf<Page>()
+
+        // //////
+        if (data == null) {
+            Log.e("JSONExt", "Data is null, cannot fetch page list")
+            return Observable.just(emptyList())
+        }
+        // //////
 
         try {
             val jsonArray = JSONArray(data)
